9764ebcd0d2d85e43f69ee1b46e5b58f5ec457e1
natlou <natloudev@gmail.com>
2022-08-15 20:37:33 -0700

    reverse queue

---
+++ stacks_queues/reverse_queue.js
@@ -0,0 +1,23 @@
+"use strict";
+const Stack = require('./Stack.js');
+function reverseK(queue, k) {
+    if (queue.isEmpty() == false) {
+        let myStack = new Stack();
+        var count = 0;
+        while (count < k) {
+            myStack.push(queue.dequeue())
+            count++;
+        }
+       
+        while (myStack.isEmpty() == false) {
+            queue.enqueue(myStack.pop());
+
+        }
+
+        for (var i = 0; i < (queue.size() - k); i++) {
+            queue.enqueue(queue.dequeue());
+        }
+    }
+
+    return queue;
+}
