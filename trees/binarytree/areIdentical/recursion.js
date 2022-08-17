69da8928e7ec47c5c9457347d6a43571e2096e6f
natlou <natloudev@gmail.com>
2022-08-16 17:58:27 -0700

    recursive solution

---
+++ trees/binarytree/areIdentical/recursion.js
@@ -0,0 +1,18 @@
+import {BinaryTree} from '../BinaryTree';
+
+let areIdentical = function(root1, root2) {
+  // TODO: Write - Your - Code
+
+  if (!root1 && !root2)
+    return true; 
+  
+  if (root1 && root2) {
+    return (
+      root1.data === root2.data &&
+      areIdentical(root1.left, root2.left) &&
+      areIdentical(root1.right, root2.right)
+    )
+  }
+
+  return false;
+};
